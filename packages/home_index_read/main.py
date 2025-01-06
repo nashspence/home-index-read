6d8ada77ac5018cf99fd558437dc8a6ecbfaa606
Nash Spence <nashspence@gmail.com>
2025-01-06 12:26:28 +0000

    conform to other home-index modules style

--- packages/home_index_read/main.py
+++ packages/home_index_read/main.py
@@ -6,7 +6,7 @@ import debugpy
 
 debugpy.listen(("0.0.0.0", 5678))
 
-if str(os.environ.get("WAIT_FOR_DEBUG_CLIENT", "false")).lower() == "true":
+if str(os.environ.get("WAIT_FOR_DEBUGPY_CLIENT", "False")) == "True":
     print("Waiting for debugger to attach...")
     debugpy.wait_for_client()
     print("Debugger attached.")
